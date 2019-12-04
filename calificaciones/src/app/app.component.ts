e3f19777a3b22e9670a1b67175abad74cdccd3da
JeffersonOmarDiaz <jefferson.diaz@epn.edu.ec>
2019-12-04 06:55:33 -0500

    diseño y crup para formulario con mysql

--- calificaciones/src/app/app.component.ts
+++ calificaciones/src/app/app.component.ts
@@ -1,10 +1,76 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import {UsuariosService} from './usuarios.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'calificaciones';
+export class AppComponent implements OnInit {
+  //title = 'calificaciones';
+  usuarios = null;
+
+  usuario = {
+    idUsuario: null,
+    nombre: null,
+    telefono: null,
+    email: null
+
+  }
+
+  constructor(private UsuariosServicio: UsuariosService){}
+
+  ngOnInit() {
+    this.obtenerUsuarios();
+  }
+
+  obtenerUsuarios(){
+    this.UsuariosServicio.obtenerUsuarios().subscribe(
+      result => this.usuarios=result
+    );
+  }
+  altaUsuario(){
+    this.UsuariosServicio.altaUsuarios(this.usuario).subscribe(
+      datos => {
+        if(datos ['resultado']=='OK'){
+          alert(datos['mensaje']);
+          this.obtenerUsuarios();
+        }
+      }
+    );
+  }
+  bajaUsuario(idUsuario){
+    this.UsuariosServicio.bajaUsuarios(idUsuario).subscribe(
+      datos => {
+        if(datos ['resultado']=='OK'){
+          alert(datos['mensaje']);
+          this.obtenerUsuarios();
+        }
+      }
+    );
+  }
+
+  editaUsuario(){
+    this.UsuariosServicio.editarUsuario(this.usuario).subscribe(
+      datos => {
+        if(datos ['resultado']=='OK'){
+          alert(datos['mensaje']);
+          this.obtenerUsuarios();
+        }
+      }
+    )
+  }
+
+  seleccionarUsuario(idUsuario){
+    this.UsuariosServicio.seleccionarUsuario(idUsuario).subscribe(
+      result => this.usuario = result[0]
+    );
+  }
+
+hayRegistros(){
+  return true;
+  }
 }
+
+
--- calificaciones/src/app/app.module.ts
+++ calificaciones/src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {FormsModule} from '@angular/forms';
+
 import { AppComponent } from './app.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import { UsuariosService } from './usuarios.service';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    UsuariosService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
